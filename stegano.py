cdd18cb4a720d360755172fa118823d65dd5b65e
Razvan-Andrei Rotaru <razvan.rotaru1601@stud.acs.upb.ro>
2023-05-24 11:03:57 +0300

    web pages left

---
+++ stegano.py
@@ -0,0 +1,69 @@
+from PIL import Image
+import numpy as np
+
+
+def stegano_decode(pixel):
+    out = list(color & 1 for color in pixel)
+    out2 = list(x for x in out)
+    return out2
+
+def get_pixels(image):
+    with Image.open(image) as img:
+        px = img.getdata()
+    px_out = list()
+    for x in px:
+        px_out.append(list(x))
+    return px_out
+
+def convert_to_char(bits):
+    chars = {tuple(map(int,f"{n:08b}")):n for n in range(256)}
+    temp = []
+    out = []
+    for i in range(8):
+        temp.append(bits[i::8])
+    unzipped = zip(*temp)
+    for b in unzipped:
+        out.append(bytes([chars[b]]).decode('ascii', errors='ignore'))
+    return ("".join(out))
+
+def stegano_decrypt(image):
+    img = Image.open(image)
+    px = img.getdata()
+    pixels = list()
+    for x in px:
+        y = list(x)
+        y.pop()
+        pixels.append(list(y))
+    decrypted_pixs = [stegano_decode(x) for x in pixels]
+    bits = [x for y in decrypted_pixs for x in y]
+    out = convert_to_char(bits)
+    return out.split('\x00')[0]
+
+def stegano_encrypt(image, message):
+    img = Image.open(image)
+    px = img.getdata()
+    pixels = list()
+    for x in px:
+        y = list(x)
+        pixels.append(list(y))
+    index = 0
+    channel = 0
+    for caracter in message:
+        for i in range(7, -1, -1):
+            pixels[index][channel] = (pixels[index][channel] & 0XFE) | ((ord(caracter) >> i) & 1)
+            channel = channel + 1
+            if channel == 3:
+                channel = 0
+                index = index + 1
+    for i in range(7, -1, -1):
+        pixels[index][channel] = (pixels[index][channel] & 0XFE) | 0
+        channel = channel + 1
+        if channel == 3:
+            channel = 0
+            index = index + 1
+    arr = np.array(pixels, dtype=np.uint8)
+    original_img_arr = arr.reshape(img.size[1], img.size[0], 4)
+    original_img_arr = np.transpose(original_img_arr, (0, 1, 2))
+    out_img = Image.fromarray(original_img_arr)
+    return out_img
+
---
+++ web_server.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request, send_file, redirect
+from werkzeug.wsgi import wrap_file
+from PIL import Image
+import base64
+import io
+import stegano
+
+app = Flask(__name__)
+
+last_encoded_img = None
+last_decoded_img = None
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/image/encode', methods = ['POST', 'GET'])
+def image_encode():
+    if request.method == "POST":
+        message = request.form['message']
+        file = request.files['file-encode']
+        out_image = stegano.stegano_encrypt(file, message)
+        buffer = io.BytesIO()
+        # Tell PIL to save as PNG into buffer
+        out_image.save(buffer, 'PNG')
+        PNG = buffer.getvalue()
+        base64_img = base64.b64encode(PNG).decode("utf-8")
+        global last_encoded_img
+        last_encoded_img = buffer.getvalue()
+
+        return render_template('encode.html', image = base64_img)
+    if request.method == "GET":
+        return render_template('encode.html', image = None)
+
+@app.route('/image/last/encoded')
+def last_encoded():
+   if last_encoded_img == None:
+       return redirect('/')
+   return send_file(io.BytesIO(last_encoded_img), as_attachment=True, download_name="last_encoded_image.png", mimetype='image/png')
+
+
+@app.route('/image/decode', methods = ['POST', 'GET'])
+def image_decode():
+    if request.method == "POST":
+        file = request.files['file-decode']
+        out = stegano.stegano_decrypt(file)
+        img = Image.open(file)
+        buffer = io.BytesIO()
+        # Tell PIL to save as PNG into buffer
+        img.save(buffer, 'PNG')
+        PNG = buffer.getvalue()
+        global last_decoded_img
+        last_decoded_img = PNG
+        return render_template('decode.html', mesaj= out)
+    if request.method == "GET":
+        return render_template('decode.html', mesaj = "")
+      
+@app.route('/image/last/decoded')
+def last_decoded():
+    if last_decoded_img == None:
+       return redirect('/')
+    return send_file(io.BytesIO(last_decoded_img), as_attachment=True, download_name="last_decoded_image.png", mimetype='image/png')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
